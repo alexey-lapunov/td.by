b1d6e025afdd879162157677582d8b299630f27e
Алексей Лапунов < >
2016-12-06 04:59:29 +0300

    db

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+//= ../../node_modules/swiper/dist/js/swiper.jquery.min.js
+//= ../../node_modules/bootstrap/dist/js/bootstrap.min.js
+
+//= partials/app.js
---
+++ src/js/partials/app.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('#slider').swiper({
+    loop: true,
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    autoplayDisableOnInteraction: false,
+    autoplay: 2500
+  });
+});
